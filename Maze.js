d763bbab68fac3f92470a5da34e42552627844a0
Ethan Mingerink <etmingerink@yahoo.com>
2024-01-24 07:56:12 -0500

    Say Start show Start, Works

--- Maze.js
+++ Maze.js
@@ -3,3 +3,10 @@
 // Date: 1.22.2024
 //Program: Maze
 // Bolt ID: BODO
+
+async function startProgram() {
+	// start maze 
+	await scrollMatrixText('Start',{ r:255,g:0,b:255}, 30, true)
+	await speak ("Start Maze", true);
+	setMainLed ({ r:255,g:0,b:255});
+}
